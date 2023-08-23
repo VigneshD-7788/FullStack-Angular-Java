c1bbe1830057b09fd4d110cd2f955ca50705fe29
VigneshD-7788 <grtvignesh7788@gmail.com>
2023-08-23 12:49:54 +0530

    Services and Dependency Injection

--- src/app/full-stack/full-stack.component.ts
+++ src/app/full-stack/full-stack.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FullStackService } from '../full-stack.service';
 
 @Component({
   selector: 'app-full-stack',
@@ -11,12 +12,14 @@ export class FullStackComponent implements OnInit {
   title: string = "Full Stack Development";
   employees: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private service: FullStackService) {
 
   }
   ngOnInit() {
-    let response = this.http.get("http://localhost:8080/api/employeesTest")
-    response.subscribe((data) => this.employees = data)
+    // let response = this.http.get("http://localhost:8080/api/employeesTest")
+    // response.subscribe((data) => this.employees = data)
+
+    this.employees = this.service.getEmployees().subscribe(data=>this.employees= data);
 
   }
 
